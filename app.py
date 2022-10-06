d4493c5fae06374258593b980d67dde09a1c26ee
Isha Choudhary <89126969+Sha-98@users.noreply.github.com>
2022-10-06 13:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def import_and_predict(image_data, model):
 image = Image.open(file)
 st.image(image, use_column_width=True)
 predictions = import_and_predict(image, model)
-class_names = ['daisy', 'dandelion', 'roses' , 'sunflowers', 'tulips']
+class_name = ['daisy', 'dandelion', 'roses' , 'sunflowers', 'tulips']
 string = "The Flower in the Image is most likely is : "+class_name[np.argmax(predictions)]
 st.success(string)
 
